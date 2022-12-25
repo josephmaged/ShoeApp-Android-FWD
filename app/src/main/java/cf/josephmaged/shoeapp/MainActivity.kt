19857b10f9a420d3d2ff2c0ee36c4a89ac4da5d4
Joseph Maged <jmaged2012@gmail.com>
2022-12-26 00:10:24 +0200

    some edit

--- app/src/main/java/cf/josephmaged/shoeapp/MainActivity.kt
+++ app/src/main/java/cf/josephmaged/shoeapp/MainActivity.kt
@@ -4,20 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import cf.josephmaged.shoeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        drawerLayout = binding.drawerLayout
+        navController = this.findNavController(R.id.nav_host_fragment_container)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
     }
 }
--- app/src/main/java/cf/josephmaged/shoeapp/screens/shoeList/ShoeListFragment.kt
+++ app/src/main/java/cf/josephmaged/shoeapp/screens/shoeList/ShoeListFragment.kt
@@ -36,7 +36,6 @@ class ShoeListFragment : Fragment(), MenuProvider {
         )
 
         binding.viewModel = viewModel
-        setHasOptionsMenu(true)
 
         viewModel.shoeList.observe(viewLifecycleOwner, Observer { shoeList ->
             for (shoe in shoeList) {
@@ -47,12 +46,14 @@ class ShoeListFragment : Fragment(), MenuProvider {
             }
         })
 
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         binding.apply {
             addShoe.setOnClickListener { view: View ->
                 Navigation.findNavController(view).navigate(R.id.action_shoeListFragment_to_shoeDetailsFragment)
             }
         }
-
         return binding.root
     }
 
@@ -63,10 +64,9 @@ class ShoeListFragment : Fragment(), MenuProvider {
 
 
     override fun onMenuItemSelected(item: MenuItem): Boolean {
-        NavigationUI.onNavDestinationSelected(
+        return NavigationUI.onNavDestinationSelected(
             item,
             requireView().findNavController()
         )
-        return true
     }
 }
